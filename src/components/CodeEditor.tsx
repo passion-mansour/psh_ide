974a36cd307f9cf577a7754c9139e3b39045d5a7
bakseungho <bakseungho@naver.com>
2024-04-05 12:30:27 +0900

    - 언어 체인지

--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -1,7 +1,37 @@
 import MonacoEditor from "@monaco-editor/react";
+import { useEffect, useRef, useState } from "react";
+import LanguageSelect from "./LanguageSelect";
+
 
 const CodeEditor = () => {
-    return <MonacoEditor theme="dark" language="javascript" height={500} />
+    const editorRef = useRef();
+    const [value, setValue] = useState('');
+    const [codeLanguage, setCodeLanguage] = useState('javascript')
+
+    const onMount = (editor: any) => {
+        editorRef.current = editor;
+    }
+
+    useEffect(() => {
+        
+    }, [codeLanguage]);
+
+    return (
+        <>
+            <LanguageSelect codeLanguage={codeLanguage} setCodeLanguage={setCodeLanguage} />
+            <MonacoEditor 
+                theme="vs-dark"
+                language={codeLanguage}
+                height={500}
+                options={{ wordWrap: 'on' }}
+                value={value}
+                onMount={
+                    onMount
+                }
+                onChange={ (value: any) => setValue(value) }
+            />
+        </>
+    )
 }
 
 export default CodeEditor;
---
+++ src/components/LanguageSelect.tsx
@@ -0,0 +1,19 @@
+const LanguageSelect = ({ codeLanguage, setCodeLanguage } : { codeLanguage: any, setCodeLanguage: any }) => {
+    const languages = ['javascript', 'c', 'java', 'python'];
+
+    const handlerChange = (e: any) => {
+        setCodeLanguage(e.target.value);
+    }
+
+    return (
+        <select onChange={ handlerChange }>
+            { languages.map( (language) => {
+                return (
+                    <option value={language}> { language } </option>
+                )
+            }) }
+        </select>
+    )
+};
+
+export default LanguageSelect;
